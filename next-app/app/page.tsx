720acde015cd5034c8d73334192fe466113bbad9
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-11-26 08:29:53 +0000

    server component

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return (<div className="text-5xl underline"> Welcome to NextJS 16</div>)
- 
-}
-export default Home
+  console.log("What type of commponent am I?");
+  return <div className="text-5xl underline"> Welcome to NextJS 16</div>;
+};
+export default Home;
