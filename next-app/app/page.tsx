a5fce9e4023811fdcf0b4e9ac9a2b8e13232bf7a
GLab <54309362+GLab-cloud@users.noreply.github.com>
2025-12-14 06:10:54 +0000

    layout - css

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-
 const Page = () => {
-  return <h1>Home page</h1>;
+  return (
+    <section>
+      <h1 className="text-center text-cyan-500">
+        The Hub for Every Dev Event You Can't Miss{" "}
+      </h1>
+      <p className="text-center mt-5">
+        {" "}
+        Hackathon, Meetups, and Conferences, All in one place{" "}
+      </p>
+    </section>
+  );
 };
 
 export default Page;
