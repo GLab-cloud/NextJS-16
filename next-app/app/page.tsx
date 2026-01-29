8ad09ec19eb5ef7cbb9b9eaa05ffbdba44538ca4
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-01-29 06:09:55 +0000

    URL

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -14,14 +14,11 @@ const Page = async () => {
   cacheLife("hours"); // fake
   let events;
   try {
-    const request = await fetch(
-      process.env.NEXT_PUBLIC_BASE_URL + "/api/events",
-      {
-        // next: { revalidate: 60 },
-        method: "GET",
-        // cache: "no-store",
-      }
-    );
+    const request = await fetch("https://next-js-16-mu.vercel.app/api/events", {
+      // next: { revalidate: 60 },
+      method: "GET",
+      // cache: "no-store",
+    });
 
     if (!request.ok) {
       if (request.status === 404) {
