e500e1c943c1fd460283541ff7fd6f1b19c5d7f0
GLab <54309362+GLab-cloud@users.noreply.github.com>
2026-01-29 04:53:45 +0000

    use CI/CD for this branch on Vercel

--- next-app/app/page.tsx
+++ next-app/app/page.tsx
@@ -1,38 +1,74 @@
-import ExploreBtn from "../components/ExploreBtn";
+import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
 import { IEvent } from "@/database";
-// import events from "@/lib/constants";
 import { cacheLife } from "next/cache";
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+import events from "@/lib/constants";
+
+// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 const Page = async () => {
   "use cache";
   cacheLife("hours");
-  const response = await fetch(`${BASE_URL}/api/events`);
-  const { events } = await response.json();
+  // let events;
+  // try {
+  //   const request = await fetch(
+  //     process.env.NEXT_PUBLIC_BASE_URL + "/api/events",
+  //     {
+  //       // next: { revalidate: 60 },
+  //       method: "GET",
+  //       // cache: "no-store",
+  //     }
+  //   );
+
+  //   if (!request.ok) {
+  //     if (request.status === 404) {
+  //       return notFound();
+  //     }
+  //     throw new Error(`Failed to fetch events: ${request.statusText}`);
+  //   }
+
+  //   const response = await request.json();
+  //   events = response.events;
+
+  //   if (!events) {
+  //     return notFound();
+  //   }
+  // } catch (error) {
+  //   console.error("Error fetching events:", error);
+  //   return notFound();
+  // }
+
   return (
-    <section>
-      <p className="text-center text-2xl font-bold">
-        The Hub for Every Dev <br /> Events You Can't Miss{" "}
-      </p>
-      <p className="text-center mt-5">
-        {" "}
-        Hackathons, Meetups, and Conferences, All in one place{" "}
-      </p>
-      <ExploreBtn />
-      <div className="mt-20 space-y-7">
-        <h3>Featured Events</h3>
-        <ul className="events">
-          {events &&
-            events.length > 0 &&
-            events.map((event: IEvent) => (
-              <li key={event.title} className="list-none">
-                {" "}
-                <EventCard {...event} />
-              </li>
-            ))}
-        </ul>
-      </div>
-    </section>
+    <main>
+      <section>
+        <p className="text-center text-2xl font-bold">
+          The Hub for Every Dev <br /> Event You Can't Miss
+        </p>
+        <p className="text-center mt-5">
+          Hackathons, Meetups, and Conferences, All in One Place
+        </p>
+
+        <ExploreBtn />
+
+        <div className="mt-20 space-y-7">
+          <h3>Featured Events</h3>
+          <Suspense fallback={<div>Loading events...</div>}>
+            <ul className="events">
+              {events &&
+                events.length > 0 &&
+                events.map((event: IEvent) => (
+                  <li key={event.title} className="list-none">
+                    <EventCard {...event} />
+                  </li>
+                ))}
+            </ul>
+          </Suspense>
+        </div>
+      </section>
+    </main>
   );
 };
 
